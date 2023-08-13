f8e025b15e737bff11b8a8d9a456dfdc75e794f1
busyapi <contact@busyapi.dev>
2023-08-13 18:13:08 +0200

    Some refactoring

---
+++ src/connection_handler.rs
@@ -0,0 +1,88 @@
+use std::env;
+use std::time::Duration;
+
+use async_std::net::{SocketAddr, TcpStream};
+use async_std::{prelude::*, task};
+use mongodb::bson::{doc, DateTime};
+use regex::Regex;
+
+use crate::mongodbclient::MongoDbClient;
+
+pub async fn handle_connection(mut stream: TcpStream, max_timeout: u8, peer: SocketAddr) {
+    let mut buffer = [0; 20];
+    let status = stream.read(&mut buffer).await;
+
+    if status.is_err() {
+        send_reponse(stream, "500 Internal Server Error").await;
+        return;
+    }
+
+    let req = std::str::from_utf8(&buffer[..]).unwrap_or("");
+
+    if req.eq("") {
+        send_reponse(stream, "400 Bad Request").await;
+        return;
+    }
+
+    let re =
+        Regex::new(r"^(GET|POST|PUT|PATCH|DELETE|OPTIONS) /(?<timeout>\d*) HTTP/1.(0|1)").unwrap();
+    let Some(caps) = re.captures(req) else {
+        send_reponse(stream, "400 Bad Request").await;
+        return;
+    };
+
+    let mut timeout: u8 = match caps["timeout"].parse() {
+        Ok(n) => n,
+        Err(_) => 0,
+    };
+
+    if timeout > max_timeout {
+        timeout = max_timeout;
+    }
+
+    if timeout > 0 {
+        task::sleep(Duration::from_secs(timeout.try_into().unwrap())).await;
+    }
+
+    send_reponse(stream, "204 No Content").await;
+
+    let mongodb_user = env::var("BUSYAPI_MONGODB_USER").unwrap_or_default();
+    let mongodb_password = env::var("BUSYAPI_MONGODB_PASSWORD").unwrap_or_default();
+    let mongodb_host = env::var("BUSYAPI_MONGODB_HOST").unwrap_or_default();
+
+    let mongo_client =
+        match MongoDbClient::new(mongodb_user, mongodb_password, mongodb_host, "busyapi").await {
+            Ok(v) => v,
+            Err(err) => {
+                eprintln!("{:?}", err);
+                return;
+            }
+        };
+
+    match mongo_client
+        .insert(
+            "requests",
+            doc! {
+                "timestamp": DateTime::now(),
+                "ipAddress": peer.ip().to_string(),
+                "timeout": u32::from(timeout)
+            },
+        )
+        .await
+    {
+        Ok(_) => (),
+        Err(err) => {
+            eprintln!("{:?}", err);
+            return;
+        }
+    };
+}
+
+async fn send_reponse(mut stream: TcpStream, status: &str) {
+    let response = format!("HTTP/1.1 {status}\r\nContent-Length: 0\r\n\r\n");
+    stream
+        .write_all(response.as_bytes())
+        .await
+        .unwrap_or_default();
+    stream.flush().await.unwrap_or_default();
+}
--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,18 @@
 extern crate derive_builder;
 
 mod config;
+mod connection_handler;
 mod mongodbclient;
 
-use async_std::net::{SocketAddr, TcpListener, TcpStream};
-use async_std::prelude::*;
-use async_std::task::{self, spawn};
+use async_std::net::TcpListener;
+use async_std::task::spawn;
 use clap::Parser;
 use config::Config;
 use futures::stream::StreamExt;
-use mongodb::bson::{doc, DateTime};
-use mongodbclient::MongoDbClient;
-use regex::Regex;
 use serde::Deserialize;
-use std::env;
 use std::fs;
-use std::time::Duration;
+
+use crate::connection_handler::handle_connection;
 
 #[derive(Parser, Debug, Deserialize)]
 pub struct Args {
@@ -82,83 +79,4 @@ fn configure() -> Config {
     conf.from_args(args);
 
     conf
-}
-
-async fn handle_connection(mut stream: TcpStream, max_timeout: u8, peer: SocketAddr) {
-    let mut buffer = [0; 20];
-    let status = stream.read(&mut buffer).await;
-
-    if status.is_err() {
-        send_reponse(stream, "500 Internal Server Error").await;
-        return;
-    }
-
-    let req = std::str::from_utf8(&buffer[..]).unwrap_or("");
-
-    if req.eq("") {
-        send_reponse(stream, "400 Bad Request").await;
-        return;
-    }
-
-    let re =
-        Regex::new(r"^(GET|POST|PUT|PATCH|DELETE|OPTIONS) /(?<timeout>\d*) HTTP/1.(0|1)").unwrap();
-    let Some(caps) = re.captures(req) else {
-        send_reponse(stream, "400 Bad Request").await;
-        return;
-    };
-
-    let mut timeout: u8 = match caps["timeout"].parse() {
-        Ok(n) => n,
-        Err(_) => 0,
-    };
-
-    if timeout > max_timeout {
-        timeout = max_timeout;
-    }
-
-    if timeout > 0 {
-        task::sleep(Duration::from_secs(timeout.try_into().unwrap())).await;
-    }
-
-    send_reponse(stream, "204 No Content").await;
-
-    let mongodb_user = env::var("BUSYAPI_MONGODB_USER").unwrap_or_default();
-    let mongodb_password = env::var("BUSYAPI_MONGODB_PASSWORD").unwrap_or_default();
-    let mongodb_host = env::var("BUSYAPI_MONGODB_HOST").unwrap_or_default();
-
-    let mongo_client =
-        match MongoDbClient::new(mongodb_user, mongodb_password, mongodb_host, "busyapi").await {
-            Ok(v) => v,
-            Err(err) => {
-                eprintln!("{:?}", err);
-                return;
-            }
-        };
-
-    match mongo_client
-        .insert(
-            "requests",
-            doc! {
-                "timestamp": DateTime::now(),
-                "ipAddress": peer.ip().to_string(),
-                "timeout": u32::from(timeout)
-            },
-        )
-        .await
-    {
-        Ok(_) => (),
-        Err(err) => {
-            eprintln!("{:?}", err);
-            return;
-        }
-    };
-}
-
-async fn send_reponse(mut stream: TcpStream, status: &str) {
-    let response = format!("HTTP/1.1 {status}\r\nContent-Length: 0\r\n\r\n");
-    stream
-        .write_all(response.as_bytes())
-        .await
-        .unwrap_or_default();
-    stream.flush().await.unwrap_or_default();
 }
